f08ac56f9f05a82bf6f1cdd24636c7c73ccac98a
s1f102202729 <s1F102202729@iniad.org>
2023-12-21 17:58:41 +0900

    諸々変更

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('chat/', include('chat.urls')),
     path('app01/', include('authtest.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
