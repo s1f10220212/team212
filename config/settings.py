34bc3d0aea2f8bbad7b18c4499d92ad6636ec1d0
s1f10220212 <s1f102202123@iniad.org>
2024-01-11 16:50:30 +0900

    chatの変更

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne'
     'chat',
     'authtest',
     'accounts',
@@ -128,3 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+ASGI_APPLICATION = "config.asgi.application"
--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('chat/', include('chat.urls')),
-    path('app01/', include('authtest.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('chat/', include("chat.urls")),
     path('admin/', admin.site.urls),
 ]
